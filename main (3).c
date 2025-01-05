252c4b88d63667373fa496741a5ee63a3db52b56
thinhsuy2908 <phan90539@gmail.com>
2025-01-05 17:21:07 +0700

    bt2ls14

---
+++ main (3).c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char thinh[]="hello word";
+    for(int i=0;i<strlen(thinh);i++){
+    printf("%c ",thinh[i]);
+    }
+
+    return 0;
+}
