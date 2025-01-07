8b0142b048163d03defd4dc63c2bff252d25fcce
thinhsuy2908 <phan90539@gmail.com>
2025-01-07 16:42:03 +0700

    bt3ls16

--- bt3ls16.cpp
+++ bt3ls16.cpp
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-int a(int *thinh, int *phan) {
-
+void a(int *thinh, int *phan,int*tong) {
+	*tong=*thinh+*phan;
 }
 int main(){
+	int tong;
 	int thinh=10,phan=50;
-	a(&thinh, &phan);
-	printf("in gia tri tong cua thinh phan la %d\n",thinh+phan);
+	a(&thinh, &phan,&tong);
+	printf("in gia tri tong cua thinh phan la %d\n",tong);
 return 0;
 }
