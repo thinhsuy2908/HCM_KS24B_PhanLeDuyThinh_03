9102c6bcf888adb391d2e0befd3920d3d0f9bb52
thinhsuy2908 <phan90539@gmail.com>
2025-01-08 22:56:17 +0700

    bt4ls18

---
+++ bt4ls18.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+struct Student {
+    char name[50];
+    int age;
+    char phoneNumber[15];
+    int id;
+};
+
+int main() {
+    struct Student student1[50];
+    for(int i=0;i<5;i++){
+    student1[i].id=i+1;	
+    printf("Nhap ten sinh vien: ");
+    fgets(student1[i].name, sizeof(student1[i].name), stdin);  
+
+    printf("Nhap tuoi sinh vien: ");
+    scanf("%d", &student1[i].age);  
+
+    printf("Nhap so dien thoai sinh vien: ");
+    scanf("%s", student1[i].phoneNumber);  
+
+    printf("ten sinh vien : %s\n", student1[i].name);
+    printf("tuoi sinh vien %d\n", student1[i].age);
+    printf("so dien thoai sinh vien: %s\n", student1[i].phoneNumber);
+    }
+    
+    return 0;
+}
